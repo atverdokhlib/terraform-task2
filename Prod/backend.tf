2b026f62760f5995c7a1657c0a283ae3154ea3b4
Andriy Tverdokhlib <a_tv@ukr.net>
2019-03-27 23:24:36 +0200

    Backend commit

---
+++ Prod/backend.tf
@@ -0,0 +1,9 @@
+terraform  {
+  backend "azurerm" {
+  container_name = "dev"
+  storage_account_name = "devprostaccount" 
+  key = "terraform.tfstate"
+  access_key = "ZKsw/xKHBUWA7SykQfewYjjAEAQ/pzh4SXJHnRUESANb7vEm96AOUqs/13gFZ5DS2B4i97PWgNw/xrLpMqdBlg=="
+  }
+
+}
